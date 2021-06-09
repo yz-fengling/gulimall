23955ffc48015f5656aa6345ca31196cb5f4ef7a
fengling <z2569126731@163.com>
2021-06-09 18:43:27 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/yz/gulinall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.gulinall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
